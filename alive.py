edd7e2adc0bd3e2140ec1076f2a94e3b78fabc41
Blair Stevens <stevens.blair.e@gmail.com>
2019-10-24 17:54:27 -0400

    exploratory

--- alive.py
+++ alive.py
@@ -1,1 +1,18 @@
-import lifetimes
+from lifetimes.utils import summary_data_from_transaction_data
+import sqlite3 as sq
+import pandas as pd
+
+db = "database/8anu.db"
+
+conn = sq.connect(db)
+cur = conn.cursor()
+cur.execute('SELECT user_id, date FROM ascent LIMIT 1000')
+
+rows = cur.fetchall()
+
+df = pd.DataFrame(rows, columns=['id','date'])
+df.dtypes
+
+summary = summary_data_from_transaction_data(df, 'id', 'date', datetime_format='s')
+
+summary
