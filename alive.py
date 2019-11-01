f08c390fb0bc65c424da87bb6cbe29eec8dddd51
Blair Stevens <stevens.blair.e@gmail.com>
2019-11-01 14:47:51 -0400

    safety commit, in progress

--- alive.py
+++ alive.py
@@ -1,4 +1,6 @@
-from lifetimes.utils import summary_data_from_transaction_data
+from lifetimes.utils import summary_data_from_transaction_data, calibration_and_holdout_data
+from lifetimes.plotting import plot_frequency_recency_matrix, plot_probability_alive_matrix, plot_period_transactions, plot_calibration_purchases_vs_holdout_purchases, plot_history_alive
+from lifetimes import BetaGeoFitter
 import sqlite3 as sq
 import pandas as pd
 
@@ -6,13 +8,35 @@ db = "database/8anu.db"
 
 conn = sq.connect(db)
 cur = conn.cursor()
-cur.execute('SELECT user_id, date FROM ascent LIMIT 1000')
+cur.execute('SELECT user_id, date FROM ascent LIMIT 2000000')
 
 rows = cur.fetchall()
 
+len(rows)
+
 df = pd.DataFrame(rows, columns=['id','date'])
-df.dtypes
+df['date'] = pd.to_datetime(df['date'], unit='s')
 
-summary = summary_data_from_transaction_data(df, 'id', 'date', datetime_format='s')
+# df
 
-summary
+conv = summary_data_from_transaction_data(df, 'id', 'date', freq='W')
+
+# conv
+
+bgf = BetaGeoFitter(penalizer_coef=1)
+bgf.fit(conv['frequency'], conv['recency'], conv['T'])
+
+bgf.summary
+bgf.params_
+
+plot = plot_frequency_recency_matrix(bgf)
+
+prob = plot_probability_alive_matrix(bgf)
+
+t = 1
+conv['predicted_purchases'] = bgf.conditional_expected_number_of_purchases_up_to_time(t, conv['frequency'], conv['recency'], conv['T'])
+conv.sort_values(by='predicted_purchases').tail(5)
+
+plot_period_transactions(bgf)
+
+summary_cal_holdout = calibration_and_holdout_data(df, 'id', 'date', calibration_period_end='2008-01-01', observation_period_end='2017-01-01' )
